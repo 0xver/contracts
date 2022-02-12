aa0795b517928ab37423489d4f7a8750a125f764
Sam <sam@shlarsen.com>
2022-02-12 06:05:49 -0500

    refactor: Add ownable constructor

--- contracts/MyFungibleToken.sol
+++ contracts/MyFungibleToken.sol
@@ -6,11 +6,9 @@ pragma solidity ^0.8.0;
  * @dev Imports contracts from the library
  */
 
-//import "./access/AccessControl.sol";
 import "./access/Ownable.sol";
 import "./erc/20/ERC20.sol";
 import "./security/ReentrancyGuard.sol";
-//import "./utils/Counters.sol";
 
 /**
  * @title My Fungible Token
@@ -18,42 +16,33 @@ import "./security/ReentrancyGuard.sol";
  * @dev Extends ERC20 fungible token standard
  */
 contract MyFungibleToken is ERC20, Ownable, ReentrancyGuard {
-    receive() external payable {}
-    fallback() external payable {}
-
     /**
-     * @dev Example events
+     * @dev Events
      */
 
-    // Stake event
     event Stake(address staker, uint256 amount, uint256 time);
-
-    // Claim event
     event Claim(address staker, uint256 amount);
-
-    // Withdrawal event
     event Withdrawal(address owner, address receiver, uint256 amount);
 
     /**
      * @dev MyFungibleToken definitions
      */
 
-    // Maps staker account to timestamp
     mapping(address => uint256) private _timestamp;
-
-    // Maps staker account to tokens staked
     mapping(address => uint256) private _staked;
 
     /**
      * @dev Sets ERC20 constructor values and mints total token supply to deployer
      */
-    constructor() ERC20("My Fungible Token", "MFT") {
+
+    constructor() ERC20("My Fungible Token", "MFT") Ownable(msg.sender) {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 
     /**
-     * @dev Example token staking function
+     * @dev Token staking function
      */
+
     function stake(uint256 amount) public gate() {
         require(balanceOf(msg.sender) > 0, "MyFungibleToken: zero token balance");
 
@@ -66,8 +55,9 @@ contract MyFungibleToken is ERC20, Ownable, ReentrancyGuard {
     }
 
     /**
-     * @dev Example token claim function
+     * @dev Token claim function
      */
+
     function claim() public gate() {
         require(_staked[msg.sender] > 0, "MyFungibleToken: no tokens staked");
         require(_timestamp[msg.sender] + 30 days <= block.timestamp, "MyFungibleToken: lock time has not completed");
@@ -85,8 +75,9 @@ contract MyFungibleToken is ERC20, Ownable, ReentrancyGuard {
     }
 
     /**
-     * @dev Example contract ether withdrawal function
+     * @dev Withdraw ether from contract
      */
+
     function withdraw(address receiver) public onlyOwner {
         (bool success, ) = payable(receiver).call{value: address(this).balance}("");
         require(success, "MyContract: ether transfer failed");
