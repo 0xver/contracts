75df0d2617ed627b11a192c3c07b98f7fe8b0034
Sam <sam@shlarsen.com>
2022-02-12 06:06:56 -0500

    refactor: Add minting and ownable constructor

--- contracts/MyNonFungibleToken.sol
+++ contracts/MyNonFungibleToken.sol
@@ -6,116 +6,111 @@ pragma solidity ^0.8.0;
  * @dev Imports contracts from the library
  */
 
-//import "./access/AccessControl.sol";
 import "./access/Ownable.sol";
-import "./erc/721/ERC721.sol";
+import "./erc/2981/ERC2981.sol";
 import "./security/ReentrancyGuard.sol";
-import "./utils/Counters.sol";
 
 /**
  * @title My Non-Fungible Token
  *
  * @dev Extends ERC721 non-fungible token standard
  */
-contract MyNonFungibleToken is ERC721, Ownable, ReentrancyGuard {
-    receive() external payable {}
-    fallback() external payable {}
-
-    // Token ID counter
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
-
+contract MyNonFungibleToken is ERC2981, Ownable, ReentrancyGuard {
     /**
-     * @dev Example events
+     * @dev Events
      */
 
-    // Mint event
     event Mint(address receiver, uint256 tokenId, string cid);
-
-    // Withdrawal event
     event Withdrawal(address caller, address receiver, uint256 amount);
 
     /**
      * @dev MyNonFungibleToken definitions
      */
 
-    // Maps account to whitelist status
     mapping(address => uint256) private _whitelist;
 
     /**
      * @dev Sets ERC721 constructor values
      */
-    constructor() ERC721("My Non-Fungible Token", "MNFT") {
+
+    constructor() ERC721("My Non-Fungible Token", "MNFT") Ownable(msg.sender) {
     }
 
     /**
-     * @dev Example withdraw ether function that emits {Withdrawal} event
+     * @dev Internal mint routing
      */
+
+    function _routeMint(address _to, string memory _cid, uint256 _percent) internal {
+        _mint(_to, _cid);
+        _setTokenRoyalty(currentTokenId(), _to, _percent);
+
+        emit Mint(_to, currentTokenId(), _cid);
+    }
+
+    /**
+     * @dev Checks if account is whitelisted
+     */
+
+    modifier whitelist(address account) {
+        require(_whitelist[account] != 0, "MyNonFungibleToken: account not in whitelist");
+
+        _;
+    }
+
+    /**
+     * @dev Adds account to whitelist
+     */
+
+    function addToWhitelist(address account) public onlyOwner {
+        require(_whitelist[account] != 1, "MyNonFungibleToken: account already in whitelist");
+
+        _whitelist[account] = 1;
+    }
+
+    /**
+     * @dev Whitelist mint function
+     */
+
+    function whitelistMint(address account, string memory cid) public whitelist(account) {
+        require(_whitelist[account] != 0, "MyNonFungibleToken: whitelist account already minted");
+
+        _whitelist[account] = 0;
+        _routeMint(account, cid, 0);
+    }
+
+    /**
+     * @dev Whitelist mint with royalty function
+     */
+
+    function whitelistMintWithRoyalty(address account, string memory cid, uint256 percent) public whitelist(account) {
+        require(_whitelist[account] != 0, "MyNonFungibleToken: whitelist account already minted");
+
+        _whitelist[account] = 0;
+        _routeMint(account, cid, percent);
+    }
+
+    /**
+     * @dev Public mint function
+     */
+    function publicMint(address account, string memory cid) public gate() {
+        _routeMint(account, cid, 0);
+    }
+
+    /**
+     * @dev Public mint with royalty function
+     */
+    function publicMintWithRoyalty(address account, string memory cid, uint256 percent) public gate() {
+        _routeMint(account, cid, percent);
+    }
+
+    /**
+     * @dev Withdraw ether from contract
+     */
+
     function withdraw(address account) public onlyOwner {
         (bool success, ) = payable(account).call{value: address(this).balance}("");
         require(success, "MyNonFungibleToken: ether transfer failed");
 
         emit Withdrawal(msg.sender, account, address(this).balance);
-    }
-
-    /**
-     * @dev Example internal event emitting mint function
-     */
-    function _minter(address account, string memory cid, uint256 percent) internal {
-        uint256 tokenId = _tokenIdCounter.current();
-        _safeMint(account, tokenId);
-        _setTokenCID(tokenId, cid);
-        _tokenIdCounter.increment();
-        _setTokenRoyalty(tokenId, account, percent);
-
-        emit Mint(account, tokenId, cid);
-    }
-
-    /**
-     * @dev Example check for whitelist modifier
-     */
-    modifier whitelist(address account) {
-        require(_whitelist[account] != 0, "MyNonFungibleToken: account not in whitelist");
-        _;
-    }
-
-    /**
-     * @dev Example add account to whitelist function
-     */
-    function addToWhitelist(address account) public onlyOwner {
-        require(_whitelist[account] != 1, "MyNonFungibleToken: account already in whitelist");
-        _whitelist[account] = 1;
-    }
-
-    /**
-     * @dev Example whitelist mint function
-     */
-    function whitelistMint(address account, string memory cid) public whitelist(account) {
-        require(_whitelist[account] != 0, "MyNonFungibleToken: whitelist account already minted");
-        _whitelist[account] = 0;
-        _minter(account, cid, 0);
-    }
-
-    /**
-     * @dev Example whitelist mint with royalty function
-     */
-    function whitelistMintWithRoyalty(address account, string memory cid, uint256 percent) public whitelist(account) {
-        require(_whitelist[account] != 0, "MyNonFungibleToken: whitelist account already minted");
-        _whitelist[account] = 0;
-        _minter(account, cid, percent);
-    }
-
-    /**
-     * @dev Example public mint function
-     */
-    function publicMint(address account, string memory cid) public gate() {
-        _minter(account, cid, 0);
-    }
-
-    /**
-     * @dev Example public mint with royalty function
-     */
-    function publicMintWithRoyalty(address account, string memory cid, uint256 percent) public gate() {
-        _minter(account, cid, percent);
     }
 }
