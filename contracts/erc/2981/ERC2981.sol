f280b20ade959237b1ae6e686a169f3fd3159660
Sam <sam@shlarsen.com>
2022-02-12 06:08:08 -0500

    refactor: Make seperate contract for royalties

---
+++ contracts/erc/2981/ERC2981.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../721/ERC721.sol";
+import "./IERC2981.sol";
+
+/**
+ * @title ERC2981 Contract
+ *
+ * @dev Implementation of the ERC2981 standard
+ */
+abstract contract ERC2981 is ERC721, IERC2981 {
+    struct RoyaltyInfo {
+        address receiver;
+        uint256 percent;
+    }
+
+    mapping(uint256 => RoyaltyInfo) internal _royalty;
+
+    /**
+     * @dev ERC2981 function
+     */
+
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice) public view virtual override returns (address, uint256) {
+        RoyaltyInfo memory royalty = _royalty[_tokenId];
+        uint256 royaltyAmount;
+
+        if (royalty.percent == 0) {
+            royaltyAmount = 0;
+        } else {
+            royaltyAmount = (_salePrice * royalty.percent) / 100;
+        }
+
+        return (royalty.receiver, royaltyAmount);
+    }
+
+    /**
+     * @dev ERC2981 internal function
+     */
+
+    function _setTokenRoyalty(uint256 _tokenId, address _receiver, uint256 _percent) internal virtual {
+        require(_percent <= 100, "ERC2981: royalty fee will exceed sale price");
+        require(_receiver != address(0), "ERC2981: royalty to the zero address");
+
+        _royalty[_tokenId] = RoyaltyInfo(_receiver, _percent);
+    }
+
+    /**
+     * @dev ERC165 support function
+     */
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC721) returns (bool) {
+        return interfaceId == type(IERC2981).interfaceId || super.supportsInterface(interfaceId);
+    }
+}
