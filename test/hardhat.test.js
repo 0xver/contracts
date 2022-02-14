cd767e393a9ee0a09461770ebc5fa81261cb12b0
Sam <sam@shlarsen.com>
2022-02-14 03:10:07 -0500

    test: Update for erc1155 feature

--- test/hardhat.test.js
+++ test/hardhat.test.js
@@ -16,14 +16,16 @@ const { ethers } = require("hardhat");
     await Token.deploy();
     var Token = await ethers.getContractFactory("MyERC721Token");
     await Token.deploy();
+    var Token = await ethers.getContractFactory("MyERC1155Token");
+    await Token.deploy();
   });
 });
 
 /**
- * @dev Tests MyERC20Token and MyERC721Token functions
+ * @dev Tests MyERC20Token functions
  */
  describe("", function () {
-  it("MyERC20Token and MyERC721Token functions", async function () {
+  it("MyERC20Token functions", async function () {
     /**
      * @dev Contract deployments
      */
@@ -35,8 +37,6 @@ const { ethers } = require("hardhat");
     var Token
     Token = await ethers.getContractFactory("MyERC20Token");
     const MyERC20Token = await Token.deploy();
-    Token = await ethers.getContractFactory("MyERC721Token");
-    const MyERC721Token = await Token.deploy();
 
     /**
      * @dev MyERC20Token function tests
@@ -80,6 +80,25 @@ const { ethers } = require("hardhat");
 
     // Token balance of addr2 should equal total supply of 1 billion tokens
     expect(await MyERC20Token.balanceOf(addr2.address)).equal(ethers.utils.parseEther("1000000000"))
+  });
+});
+
+/**
+ * @dev Tests MyERC721Token functions
+ */
+ describe("", function () {
+  it("MyERC721Token functions", async function () {
+    /**
+     * @dev Contract deployments
+     */
+
+    // Gets owner address and second address
+    const [addr1, addr2, addr3] = await ethers.getSigners();
+
+    // Deploys tokens
+    var Token
+    Token = await ethers.getContractFactory("MyERC721Token");
+    const MyERC721Token = await Token.deploy();
 
     /**
      * @dev MyERC721Token function tests
@@ -126,6 +145,100 @@ const { ethers } = require("hardhat");
 
     // Token balance for addr3 should equal 2
     expect(await MyERC721Token.balanceOf(addr3.address)).equal(2)
+  });
+});
+
+/**
+ * @dev Tests MyERC1155Token functions
+ */
+ describe("", function () {
+  it("MyERC1155Token functions", async function () {
+    /**
+     * @dev Contract deployments
+     */
+
+    // Gets owner address and second address
+    const [addr1, addr2, addr3, addr4] = await ethers.getSigners();
+
+    // Deploys tokens
+    var Token
+    Token = await ethers.getContractFactory("MyERC1155Token");
+    const MyERC1155Token = await Token.deploy();
+
+    /**
+     * @dev MyERC1155Token function tests
+     */
+
+    // Token balance #1 for addr1 should equal 0
+    expect(await MyERC1155Token.balanceOf(addr1.address, 1)).equal(0)
+
+    // Initial mint 500 tokens for token ID #1
+    await MyERC1155Token.initialMint(addr1.address, "QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1", 500)
+
+    // Token balance #1 for addr1 should equal 500
+    expect(await MyERC1155Token.balanceOf(addr1.address, 1)).equal(500)
+
+    // Transfer 200 tokens of token ID #1 from addr1 to addr2
+    await MyERC1155Token.safeTransferFrom(addr1.address, addr2.address, 1, 200, 0x0)
+
+    // Token balance #1 for addr1 should equal 300
+    expect(await MyERC1155Token.balanceOf(addr1.address, 1)).equal(300)
+
+    // Token balance #1 for addr2 should equal 200
+    expect(await MyERC1155Token.balanceOf(addr2.address, 1)).equal(200)
+
+    // Approve addr3 to spend the tokens of addr2
+    await MyERC1155Token.connect(addr2).setApprovalForAll(addr3.address, true)
+
+    // Check addr3 to spend the tokens of addr2
+    expect(await MyERC1155Token.isApprovedForAll(addr2.address, addr3.address)).equal(true)
+
+    // Transfer 200 tokens of token ID #1 from addr1 to addr2
+    await MyERC1155Token.connect(addr3).safeTransferFrom(addr2.address, addr3.address, 1,100, 0x0)
+
+    // Mint more tokens for ID #1 to addr4
+    await MyERC1155Token.mint(addr4.address, 600)
+
+    // Initial mint 2500 tokens for token ID #2
+    await MyERC1155Token.initialMint(addr4.address, "QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/2", 2500)
+
+    // Token balance #1 for addr3 should equal 100
+    expect(await MyERC1155Token.balanceOf(addr3.address, 1)).equal(100)
+
+    // Token balance #1 for addr4 should equal 600
+    expect(await MyERC1155Token.balanceOf(addr4.address, 1)).equal(600)
+
+    // Token balance #2 for addr4 should equal 2500
+    expect(await MyERC1155Token.balanceOf(addr4.address, 2)).equal(2500)
+
+    // Token URI for #1 should be correct identifier
+    expect(await MyERC1155Token.uri(1)).equal("ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1")
+
+    // Token URI for #2 should be correct identifier
+    expect(await MyERC1155Token.uri(2)).equal("ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/2")
+  });
+});
+
+/**
+ * @dev Checks for ERC165 support for MyERC721Token
+ */
+ describe("", function () {
+  it("ERC165 support for MyERC721Token", async function () {
+    /**
+     * @dev Contract deployments
+     */
+
+    // Deploys tokens
+    var Token
+    Token = await ethers.getContractFactory("MyERC721Token");
+    const MyERC721Token = await Token.deploy();
+
+    /**
+     * @dev Check for ERC165 support
+     */
+
+    // Should return true
+    expect(await MyERC721Token.supportsInterface(ethers.utils . hexlify(0x01ffc9a7))).equal(true)
   });
 });
 
@@ -222,24 +335,93 @@ describe("", function () {
 });
 
 /**
- * @dev Checks for ERC165 support for MyERC721Token
+ * @dev Checks for ERC165 support for MyERC1155Token
  */
  describe("", function () {
-  it("ERC165 support for MyERC721Token", async function () {
+  it("ERC165 support for MyERC1155Token", async function () {
     /**
      * @dev Contract deployments
      */
 
     // Deploys tokens
     var Token
-    Token = await ethers.getContractFactory("MyERC721Token");
-    const MyERC721Token = await Token.deploy();
+    Token = await ethers.getContractFactory("MyERC1155Token");
+    const MyERC1155Token = await Token.deploy();
 
     /**
      * @dev Check for ERC165 support
      */
 
     // Should return true
-    expect(await MyERC721Token.supportsInterface(ethers.utils . hexlify(0x01ffc9a7))).equal(true)
+    expect(await MyERC1155Token.supportsInterface(ethers.utils . hexlify(0x01ffc9a7))).equal(true)
+  });
+});
+
+/**
+ * @dev Checks for ERC1155 support for MyERC1155Token
+ */
+ describe("", function () {
+  it("ERC1155 support for MyERC1155Token", async function () {
+    /**
+     * @dev Contract deployments
+     */
+
+    // Deploys tokens
+    var Token
+    Token = await ethers.getContractFactory("MyERC1155Token");
+    const MyERC1155Token = await Token.deploy();
+
+    /**
+     * @dev Check for ERC1155 support
+     */
+
+    // Should return true
+    expect(await MyERC1155Token.supportsInterface(ethers.utils . hexlify(0xd9b67a26))).equal(true)
+  });
+});
+
+/**
+ * @dev Checks for ERC1155Metadata support for MyERC1155Token
+ */
+ describe("", function () {
+  it("ERC1155Metadata support for MyERC1155Token", async function () {
+    /**
+     * @dev Contract deployments
+     */
+
+    // Deploys tokens
+    var Token
+    Token = await ethers.getContractFactory("MyERC1155Token");
+    const MyERC1155Token = await Token.deploy();
+
+    /**
+     * @dev Check for ERC1155Metadata support
+     */
+
+    // Should return true
+    expect(await MyERC1155Token.supportsInterface(ethers.utils . hexlify(0x0e89341c))).equal(true)
+  });
+});
+
+/**
+ * @dev Checks for ERC1155Receiver support for MyERC1155Token
+ */
+ describe("", function () {
+  it("ERC1155Receiver support for MyERC1155Token", async function () {
+    /**
+     * @dev Contract deployments
+     */
+
+    // Deploys tokens
+    var Token
+    Token = await ethers.getContractFactory("MyERC1155Token");
+    const MyERC1155Token = await Token.deploy();
+
+    /**
+     * @dev Check for ERC1155Receiver support
+     */
+
+    // Should return true
+    expect(await MyERC1155Token.supportsInterface(ethers.utils . hexlify(0x4e2312e0))).equal(true)
   });
 });
