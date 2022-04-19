bc6f78294e284bc2508a95cabaca8964c7e3fe5e
Sam <sam@shlarsen.com>
2022-04-19 03:58:31 -0400

    feat: Supports interface for erc20

--- contracts/packages/Bundle_ERC20.sol
+++ contracts/packages/Bundle_ERC20.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 
+import "../erc/165/ERC165.sol";
 import "./20/Package_ERC20.sol";
 import "./173/Package_ERC173.sol";
 import "../security/Guardian.sol";
@@ -9,6 +10,12 @@ import "../security/Guardian.sol";
 /**
  * @dev Supports interface bundle
  */
-contract Bundle is Package_ERC20, Package_ERC173, Guardian {
+contract Bundle is Package_ERC20, Package_ERC173, Guardian, ERC165 {
     constructor() Package_ERC20("My ERC20 Token", "TKN") Package_ERC173(msg.sender) {}
+
+    function supportsInterface(bytes4 interfaceId) public pure override(ERC165) returns (bool) {
+        return
+            interfaceId == type(ERC165).interfaceId ||
+            interfaceId == type(ERC173).interfaceId;
+    }
 }
